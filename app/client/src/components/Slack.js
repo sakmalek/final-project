88e0d58a97d8bd462b433057c773a7e82572e462
abdelmalek.saksioui <abdelmalek.saksioui@cgm.com>
2021-12-12 12:35:20 +0100

    added bg image to body

--- app/client/src/components/Slack.js
+++ app/client/src/components/Slack.js
@@ -1,27 +1,26 @@
 import React from "react";
-import {Box, Paper} from "@mui/material";
+import {Grid, Paper} from "@mui/material";
 
 const Slack = () => {
 
     return (
-        <Box
-            sx={{
-                display: 'flex',
-                flexDirection: "row",
-                '& > :not(style)': {
-                    m: 1,
-                    height: '95vh',
-                },
-            }}
-        >
+        <Grid sx={{m: 1, p: 1}} item xs={12}
+              spacing={2}
+              container
+              minHeight="100vh"
+              minWidth="100vw">
 
-            <Paper style={{width: '20%'}} elevation={3}>
-                <h2>Channels</h2>
-            </Paper>
-            <Paper style={{width: '80%'}} elevation={3}>
-
-            </Paper>
-        </Box>
+            <Grid item xs={4}>
+                <Paper elevation={3}>
+                    <h2>Channels</h2>
+                </Paper>
+            </Grid>
+            <Grid item xs={8}>
+                <Paper elevation={3}>
+                    <h2>Chat</h2>
+                </Paper>
+            </Grid>
+        </Grid>
     );
 }
 
